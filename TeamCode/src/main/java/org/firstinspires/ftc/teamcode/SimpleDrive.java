b5f5ef7ec6631660c087d5776ed84158230c1a0d
STOKERobotics <robotics@stokemt.org>
2025-10-30 19:39:19 -0600

    mostly merged simpledrive and april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,9 +14,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
 
 @TeleOp(name = "SimpleDriveJava")
 public class SimpleDrive extends LinearOpMode {
+
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    /**
+     * The variable to store our instance of the AprilTag processor.
+     */
+    private AprilTagProcessor aprilTag;
+
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private VisionPortal visionPortal;
+
 
     private DcMotor motor0;
     private DcMotor motor1;
@@ -49,8 +73,14 @@ public class SimpleDrive extends LinearOpMode {
      */
     @Override
     public void runOpMode() {
-        int motor22;
-        int motor02;
+        initAprilTag();
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+
+
         BNO055IMU.Parameters imuParameters;
 
 
@@ -59,8 +89,8 @@ public class SimpleDrive extends LinearOpMode {
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
         motor2 = hardwareMap.get(DcMotor.class, "motor2");
         motor3 = hardwareMap.get(DcMotor.class, "motor3");
-        //motor0b = hardwareMap.get(DcMotor.class, "motor0b");
-        //motor1b = hardwareMap.get(DcMotor.class, "motor1b");
+        motor0b = hardwareMap.get(DcMotor.class, "motor0b");
+        motor1b = hardwareMap.get(DcMotor.class, "motor1b");
         servo0 = hardwareMap.get(Servo.class, "servo0");
         servo1 = hardwareMap.get(CRServo.class, "servo1");
         imu1 = hardwareMap.get(BNO055IMU.class, "imu 1");
@@ -123,9 +153,104 @@ public class SimpleDrive extends LinearOpMode {
         }
     }
 
-    /**
-     * Describe this function...
-     */
+    private void initAprilTag() {
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+        aprilTag = new AprilTagProcessor.Builder()
+
+                // The following default settings are available to un-comment and edit as needed.
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+                // ... these parameters are fx, fy, cx, cy.
+
+                .build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        //aprilTag.setDecimation(3);
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(1280, 800));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }   // end method initAprilTag()
+
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }   //
 
     /**
      * Describe this function...
@@ -147,7 +272,6 @@ public class SimpleDrive extends LinearOpMode {
         telemetry.addData("VelMotor3", ((DcMotorEx) motor3).getVelocity());
         telemetry.update();
     }
-
 
     private void buttons() {
         // Run motor0b at full power (1) when button A is pressed, stop when released
@@ -194,7 +318,6 @@ public class SimpleDrive extends LinearOpMode {
 
     }
 
-
     /**
      * Describe this function...
      */
@@ -223,45 +346,50 @@ public class SimpleDrive extends LinearOpMode {
         sticks2();
     }
 
-
-
-/**
-     * Gyro correct values. Code from ChatGTP created code.
-     */
-    private void sticks4() {
-        YawValue = Math.round(imu1.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle) * (Math.PI / 180);
-        correctedStrafePower = strafePower * Math.cos(YawValue / 180 * Math.PI) - drivePower * Math.sin(YawValue / 180 * Math.PI);
-        correctedDrivePower = drivePower * Math.cos(YawValue / 180 * Math.PI) + strafePower * Math.sin(YawValue / 180 * Math.PI);
-        drive2();
-    }
+    /**
+         * Gyro correct values. Code from ChatGTP created code.
+         */
+        private void sticks4() {
+            YawValue = Math.round(imu1.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle) * (Math.PI / 180);
+            correctedStrafePower = strafePower * Math.cos(YawValue / 180 * Math.PI) - drivePower * Math.sin(YawValue / 180 * Math.PI);
+            correctedDrivePower = drivePower * Math.cos(YawValue / 180 * Math.PI) + strafePower * Math.sin(YawValue / 180 * Math.PI);
+            drive2();
+        }
 
     /**
-     * Send corrected values to motors
-     */
-    private void drive2() {
-        // Front right motor
-        motor0.setPower((correctedDrivePower - correctedStrafePower) - rotatePower);
-        // Front left motor
-        motor2.setPower(correctedDrivePower + correctedStrafePower + rotatePower);
-        // Back right motor
-        motor3.setPower((correctedDrivePower - correctedStrafePower) + rotatePower);
-        // Back left motor
-        motor1.setPower((correctedDrivePower + correctedStrafePower) - rotatePower);
-    }
+         * Send corrected values to motors
+         */
+        private void drive2() {
+            // Front right motor
+            motor0.setPower((correctedDrivePower - correctedStrafePower) - rotatePower);
+            // Front left motor
+            motor2.setPower(correctedDrivePower + correctedStrafePower + rotatePower);
+            // Back right motor
+            motor3.setPower((correctedDrivePower - correctedStrafePower) + rotatePower);
+            // Back left motor
+            motor1.setPower((correctedDrivePower + correctedStrafePower) - rotatePower);
+        }
 
     private void lights(){
-        if (gamepad2.a && !wasButtonAPressed) { // Detect button press (not hold)
-            isRedOn = !isRedOn;                 // Toggle the state
-            redLED.setState(isRedOn);           // Update LED state
-        }
-        wasButtonAPressed = gamepad2.x;         // Remember the button state
+            if (gamepad2.a && !wasButtonAPressed) { // Detect button press (not hold)
+                isRedOn = !isRedOn;                 // Toggle the state
+                redLED.setState(isRedOn);           // Update LED state
+            }
+            wasButtonAPressed = gamepad2.x;         // Remember the button state
 
 
-        if (gamepad2.b && !wasButtonBPressed) { // Detect button press (not hold)
-            isBlueOn = !isBlueOn;               // Toggle the state
-            blueLED.setState(isBlueOn);         // Update LED state
+            if (gamepad2.b && !wasButtonBPressed) { // Detect button press (not hold)
+                isBlueOn = !isBlueOn;               // Toggle the state
+                blueLED.setState(isBlueOn);         // Update LED state
+            }
+            wasButtonBPressed = gamepad2.y;         // Remember the button state
         }
-        wasButtonBPressed = gamepad2.y;         // Remember the button state
-    }
 }
+/**
+     * Describe this function...
+     */
+
+
+
+
 
